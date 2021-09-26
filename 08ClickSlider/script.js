91cae811740b35cda4f1983d91658e26fce999ca
DogukanSAVA <dogukan98sv@hotmail.com>
2021-09-26 12:50:26 +0300

    click slider end

---
+++ 08ClickSlider/script.js
@@ -0,0 +1,33 @@
+const slider = document.querySelector('.items');
+let isDown = false
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown',(e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave',() => {
+    isDown = false;
+    slider.classList.remove('active');
+    console.log("mouseleave")
+});
+
+slider.addEventListener('mouseup',() => {
+    isDown = false;
+    slider.classList.remove('active');
+    console.log("mouseup")
+});
+
+slider.addEventListener('mousemove',(e) => {
+    if(!isDown) return;
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2;
+    slider.scrollLeft = scrollLeft - walk;
+    
+    e.preventDefault()
+
+});
